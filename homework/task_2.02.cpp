2c993f2d570acf1bd3cbfa99c2e2cd0c5d7d1a39
Vadim <svdana06@gmail.com>
2025-09-10 15:55:40 +0300

    2.02

---
+++ homework/task_2.02.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+  double const epsilon = 10e-6;
+  double a, b, c;
+  std::cin >> a >> b >> c;
+  if (std::abs(a) < epsilon)
+  {
+    if (std::abs(b) > epsilon) 
+    {
+      double result = -c / b;
+      std::cout << "Решение линейного уравнения: " << result << '\n';
+    }
+    else 
+    {
+      std::cout << "Данное уравнение не имеет решения" << '\n';
+    }
+  }
+  else
+  {
+    double D = b*b - 4 * a * c;
+    if (D < 0)
+    {
+      std::cout << "Данное уравнение не имеет решения" << '\n';
+    }
+    else if (D < epsilon)
+    {
+      double x = -b / (2 * a);
+      std::cout << "Корень квадратного уравнения: " << x << '\n';
+    }
+    else
+    {
+      double x1 = (-b - std::sqrt(D)) / (2 * a);
+      double x2 = (-b + std::sqrt(D)) / (2 * a);
+      std::cout << "Решение квадратного уравнения: " << x1 << " " << x2 << '\n';
+    }
+  }
+}
