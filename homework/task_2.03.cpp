f714c98a072476bd954df3c8ce5fb1b6a5089a70
Vadim <svdana06@gmail.com>
2025-09-16 12:17:05 +0000

    task_2.03 is done

--- homework/task_2.03.cpp
+++ homework/task_2.03.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 
-int main() 
+int main()
 {
-  int num;
-  std::cin >> num;
-  switch (int y = num; y)
+  double const epsilon = 10e-6;
+  double a, b, c;
+  std::cin >> a >> b >> c;
+  if (std::abs(a) < epsilon)
   {
-    case 32: { std::cout << "Это цифра"; }
-
-    case 47: { std::cout << "Это ,erdf"; }
+    if (std::abs(b) > epsilon) 
+    {
+      double result = -c / b;
+      std::cout << "Решение линейного уравнения: " << result << '\n';
+    }
+    else 
+    {
+      std::cout << "Данное уравнение не имеет решения" << '\n';
+    }
+  }
+  else
+  {
+    double D = b*b - 4 * a * c;
+    if (D < 0)
+    {
+      std::cout << "Данное уравнение не имеет решения" << '\n';
+    }
+    else if (D < epsilon)
+    {
+      double x = -b / (2 * a);
+      std::cout << "Корень квадратного уравнения: " << x << '\n';
+    }
+    else
+    {
+      double x1 = (-b - std::sqrt(D)) / (2 * a);
+      double x2 = (-b + std::sqrt(D)) / (2 * a);
+      std::cout << "Решение квадратного уравнения: " << x1 << " " << x2 << '\n';
+    }
   }
 }
